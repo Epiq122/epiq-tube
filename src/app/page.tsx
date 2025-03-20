15a75ca2eeb012a9a322758fbd5c0f3ac734942a
Robert Gleason <l2ob122@gmail.com>
2025-03-19 18:56:49 -0700

    Created the basic layout for the HOMESECTION, including nav bar and sidebars, created module basic folder structure to keep components in a clean area

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className='flex h-screen w-screen items-center justify-center text-rose-500'>
-      <h1 className='text-4xl font-bold'>Welcome to the show!</h1>
-      <Button variant='outline'>Click me</Button>
-    </div>
-  );
-}
