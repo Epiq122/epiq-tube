6b950b5b4b20fbfab4eba9bd133d2ac7d71a4464
Robert Gleason <l2ob122@gmail.com>
2025-03-19 19:03:47 -0700

    Add HomeSidebar and AuthButton components; update HomeNavbar and HomeLayout to include sidebar and authentication button, removed transitions from the sidebar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return (
-    <div>
-      <Image src='/logo.svg' alt='logo' width={50} height={50} />
-      <p className='text-xl font-bold tracking-tight'>EpiqTube</p>
-    </div>
-  );
+  return <div>I will load videos in the future</div>;
 }
---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from '@/components/ui/sidebar';
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const items = [
+  {
+    title: 'History',
+    url: '/playlists/history',
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: 'Liked Videos',
+    url: '/playlists/liked',
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: 'All Playlists',
+    url: '/playlists',
+    icon: ListVideoIcon,
+    auth: true,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild
+                tooltip={item.title}
+                isActive={false} //TODO: Change to look at current pathname
+                onClick={() => {}} //TODO: DO something on click
+              >
+                <Link href={item.url} className='flex items-center gap-4'>
+                  <item.icon />
+                  <span className='text-sm'>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
